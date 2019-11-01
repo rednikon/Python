cfa4bbe46430313472c7e1355fe7eb2a2b06b53d
Vee McClure <30158376+rednikon@users.noreply.github.com>
2019-10-31 21:39:56 -0700

    Add files via upload

---
+++ Book-Exercises/The-Self-Taught-Programmer/self-taught-chapter5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 18:15:46 2019
+
+@author: veemac
+
+
+Solutions to Chapter 5 Exercises
+
+"""
+
+# 5.1 Create a list of your favorite musicians.
+
+musicians = ("Breaking Benjamin", "Walk the Moon", "Michael Franti", "Jonny Lang", "Fall Out Boy", "Marilyn Manson", "Placebo")
+
+# 5.2 Create a list of tuples, with each tuple containing the longitude and latitude of places you've visited. 
