b84b39b154252c3be821889ec01e8219118daad7
Vee McClure <rednikon@yahoo.com>
2020-05-28 15:32:54 -0700

    updated function names

--- Book-Exercises/The-Self-Taught-Programmer/self-taught1.py
+++ Book-Exercises/The-Self-Taught-Programmer/self-taught1.py
@@ -10,51 +10,50 @@ print("The Self-Taught Programmer Exercises")
 
 
 # 2. Print a message if a variable is less than 10, and another message if the variable is equal or greater than 10
-var_one = input("Please enter a whole number: ")
-x = int(var_one)
+get_variable_one = input("Please enter a whole number: ")
+variable_one = int(get_variable_one)
 
-if x < 10:
+if variable_one < 10:
     print("The variable you entered is less than than 10.")
-else: 
+else:
     print("The variable you entered is 10 or greater.")
 
 
 # 3. Print a message if a variable is equal or less than 10, another if it's greater than 10 but less than or equal to 25, and the last if it's greater than 25
-var_two = input("Please enter another whole number: ")
-y = int(var_two)
+get_variable_two = input("Please enter another whole number: ")
+variable_two = int(get_variable_two)
 
 # use if-elif-else ladder since there are 3 conditions
-if  y <= 10:
+if  variable_two <= 10:
     print("The variable you entered is less than or equal to 10.")
-elif 10 < y <= 25:
+elif 10 < variable_two <= 25:
     print("The variable you entered is between 10 and 25.")
 else:
     print("The variable you entered is greater than 25.")
 
-# 4. Create a program that divides two variables and prints the remainder. 
+# 4. Create a program that divides two variables and prints the remainder.
 <<<<<<< HEAD
 print("\nLet's divide some numbers.")
-numerator = input("Please enter a numerator: ")
-n = int(numerator)
+get_numerator = input("Please enter a numerator: ")
+n = int(get_numerator)
 
-denominator = input("Please enter a denominator: ")
-d = int(denominator)
+get_denominator = input("Please enter a denominator: ")
+d = int(get_denominator)
 
 print("The remainder is: ", n%d)
 
-# 5. Create a program that takes two variables, divides them, and prints the quotient. 
+# 5. Create a program that takes two variables, divides them, and prints the quotient.
 print("The quotient is: ", n/d)
-    
-# 6. Write a program with a variable 'age' assigned to an integer that prints different strings depending on what ingeger 'age' is
-age = input("What's your age? ")
-a = int(age)
 
-if  a <= 18:
+# 6. Write a program with a variable 'age' assigned to an integer that prints different strings depending on what ingeger 'age' is
+get_age = input("What's your age? ")
+age = int(get_age)
+
+if  age <= 18:
     print("Stay in school.")
 
-elif 18 < a < 35:
+elif 18 < age < 35:
     print("Enjoy your youth.")
 
 else:
     print("You're hitting your stride.")
-
