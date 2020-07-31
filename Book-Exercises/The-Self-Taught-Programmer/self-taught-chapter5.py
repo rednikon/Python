d434d93a8043a5b1a88a162e0d51f99e51372214
Vee McClure <rednikon@yahoo.com>
2020-07-30 19:34:04 -0700

    Chapter 5 exercises

--- Book-Exercises/The-Self-Taught-Programmer/self-taught-chapter5.py
+++ Book-Exercises/The-Self-Taught-Programmer/self-taught-chapter5.py
@@ -11,7 +11,12 @@ Solutions to Chapter 5 Exercises
 """
 
 # 5.1 Create a list of your favorite musicians.
+favorite_musicians = ("Breaking Benjamin", "Walk the Moon", "Michael Franti", "Jonny Lang", "Fall Out Boy", "Marilyn Manson", "Placebo")
 
-musicians = ("Breaking Benjamin", "Walk the Moon", "Michael Franti", "Jonny Lang", "Fall Out Boy", "Marilyn Manson", "Placebo")
+# 5.2 Create a list of tuples, with each tuple containing the longitude and latitude of places you've visited.
+previous_residences = [(8.3405, 115.0920), (52.5200, 13.4050), (20.4230, 86.9223)]
 
-# 5.2 Create a list of tuples, with each tuple containing the longitude and latitude of places you've visited. 
+# 5.3 Create a dictionary that contains different attributes about you: height, favorite color, favorite author, etc
+my_personal_details = ['Name': 'Vee', 'Favorite Color': 'Green', 'Favorite Author': 'William Shakespeare', 'Favorite Hobbies': 'Writing, Motorcycles, SCUBA']
+
+# 5.4 Write a program that let's the user ask about your personal details and returns the results you gave in the previous exercise.
